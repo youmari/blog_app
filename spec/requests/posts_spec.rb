35dbb5a0da3da64fa86f16991a5e511b8a5549f5
youmari <youmari300@gmail.com>
2022-03-23 17:41:58 +0100

    Write tests for posts controller requests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
+RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    before(:each) { get users_path }
+    before(:each) { get user_posts_path(1) }
     it 'should match ok status' do
       expect(response).to have_http_status(:ok)
     end
     it 'should the body has h1 text' do
-      expect(response.body).to include('Here is a list of users')
+      expect(response.body).to include('Here is a list of posts for given specific user')
     end
     it 'should render index template' do
       expect(response).to render_template(:index)
     end
   end
   describe 'GET /show' do
-    before(:each) { get user_path(1) }
+    before(:each) { get user_post_path(1, 3) }
     it 'should match ok status' do
       expect(response).to have_http_status(:ok)
     end
     it 'should the body has h1 text' do
-      expect(response.body).to include('Here is a list of a specific user')
+      expect(response.body).to include('Here is a list of post for given specific user')
     end
     it 'should render index template' do
       expect(response).to render_template(:show)
