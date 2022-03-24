fe39004827dac55af7e9e603cf8bf61e6c374e24
youmari <youmari300@gmail.com>
2022-03-24 07:42:30 +0100

    Create Comment model with migration and test file

---
+++ db/migrate/20220324064108_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.references :authorId, null: false, foreign_key: true
+      t.references :postId, null: false, foreign_key: true
+      t.string :Text
+
+      t.timestamps
+    end
+  end
+end
