0629f3c47e3b0b933fce71779b003cfe393d5200
youmari <youmari300@gmail.com>
2022-03-28 08:32:47 +0000

    create users and posts and custom method to get three most recent posts from a user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ApplicationRecord
-  belongs_to :author_id
+  belongs_to :author, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
+
+  scope :three_recent_posts, ->(user) { user.posts.order(id: :DESC).limit(3) }
 end
