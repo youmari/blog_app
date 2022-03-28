cb3386a783b7fdd1e6902b0659132379d6cefde0
youmari <youmari300@gmail.com>
2022-03-28 10:00:33 +0000

    add a method to update likes counter for a post

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  scope :update_likes_counter, ->(post) { post.update(likes_counter: Like.all.where("post_id = #{post.id}").count) }
 end
