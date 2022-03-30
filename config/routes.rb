8441174d385f2154fbc281f51c03231395d44fab
youmari <youmari300@gmail.com>
2022-03-30 05:25:16 +0000

    Add from to add post and saving it

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   resources :comments
   resources :likes
-  resources :posts
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create]
   end
 end
