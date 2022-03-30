5d87b31b6d842eac1d6107e70a571367f18dd5b1
youmari <youmari300@gmail.com>
2022-03-30 07:18:31 +0000

    Add like functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :likes
   root "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments 
+      resources :likes
     end
   end
 end
